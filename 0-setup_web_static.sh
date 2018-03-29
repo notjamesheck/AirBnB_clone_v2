22d580e2232f0b5bab29a501ea6e2be30206b3fd
Henry Hsu <h_hsu817@yahoo.com>
2018-03-29 16:34:27 +0000

    Edited error in sed insertion line

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,11 +7,11 @@ sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
 
-echo "Code: 451" | sudo tee /data/web_static/releases/test/index.html
+echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
 
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 
-sudo sed -i "58i \\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n\n\n" /etc/nginx/sites-enabled/default
+sudo sed -i "39i \\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n\n\n" /etc/nginx/sites-enabled/default
 
 sudo service nginx restart
