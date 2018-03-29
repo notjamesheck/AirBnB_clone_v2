3fd128d49489ea499a6a92af9853fd5d4c94cf03
Henry Hsu <h_hsu817@yahoo.com>
2018-03-29 16:00:35 +0000

    Task 0 first pass

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# Sets up my web servers for the deployment of web_static
+
+sudo apt-get update
+sudo apt-get -y install nginx
+
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+
+echo "Code: 451" | sudo tee /data/web_static/releases/test/index.html
+
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+
+sudo sed -i "58i \\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n\n\n" /etc/nginx/sites-enabled/default
+
+sudo service nginx restart
