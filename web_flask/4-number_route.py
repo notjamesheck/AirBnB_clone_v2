59940f9baf1537c3e7760335fc182a7986257be0
notjamesheck <224@holbertonschool.com>
2018-04-18 14:15:23 -0700

    4

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -21,22 +21,22 @@ def web_flask():
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_route(text):
-    text.replace(" ", "_")
-    return 'C {}'.format(text)
+    text = text.replace("_", " ")
+    return 'C %s' % text
 
 
+@app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python_route(text=None):
-    if text is None:
-        text = 'is cool'
-    text.replace(" ", "_")
-    return 'Python {}'.format(text)
+def python_route(text='is cool'):
+    text = text.replace("_", " ")
+    return 'Python %s' % text
 
 
+@app.route('/number/', strict_slashes=False)
 @app.route('/number/<int:n>', strict_slashes=False)
 def number_route(n):
     if type(n) is int:
-        return '{} is a number'.format(text)
+        return '%d is a number' % n
 
 
 if __name__ == '__main__':
