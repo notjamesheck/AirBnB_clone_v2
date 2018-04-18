04d4fae034ab99590d8027450a562bacf62684cf
notjamesheck <224@holbertonschool.com>
2018-04-17 21:00:59 -0700

    2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+'''
+    comment
+'''
+from flask import Flask
+app = Flask(__name__)
+'''
+    comment
+'''
+
+
+@app.route('/', strict_slashes=False)
+def hello_route():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def web_flask():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route():
+    return 'C %s' % text
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
