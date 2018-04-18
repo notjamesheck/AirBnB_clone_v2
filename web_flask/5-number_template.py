5fbd22cce96bd8cfa055834da97afb443fae2dec
notjamesheck <224@holbertonschool.com>
2018-04-18 14:41:03 -0700

    ssss

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -38,7 +38,6 @@ def number_route(n):
     return '%d is a number' % n
 
 
-@app.route('/number_template/')
 @app.route('/number_template/<n>')
 def number_template(n):
     return render_template('5-number.html', n=n)
