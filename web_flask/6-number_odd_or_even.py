75213add67fa351ee30c05f5509af975bdea2df9
notjamesheck <224@holbertonschool.com>
2018-04-18 16:16:52 -0700

    ssf

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -43,7 +43,7 @@ def number_template(n):
     return render_template('5-number.html', n=n)
 
 
-@app.route('/number_odd_or_even/<int:n>')
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_odd_or_even(n):
     return render_template('6-number_odd_or_even.html', n=n, )
 
