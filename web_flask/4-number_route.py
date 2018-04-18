832db643ac5836244dabeba3ec09390c84ae8d97
notjamesheck <224@holbertonschool.com>
2018-04-18 13:59:04 -0700

    slash

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -25,7 +25,7 @@ def c_route(text):
     return 'C {}'.format(text)
 
 
-@app.route('/python/(<text>)', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
 def python_route(text=None):
     if text is None:
         text = 'is cool'
