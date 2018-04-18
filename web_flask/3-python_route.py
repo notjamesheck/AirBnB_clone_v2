f03ccd7b63dd2c9a719d06eeebf3154076510640
notjamesheck <224@holbertonschool.com>
2018-04-18 14:05:35 -0700

    wat

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -27,9 +27,7 @@ def c_route(text):
 
 @app.route("/python/", strict_slashes=False)
 @app.route('/python/(<text>)', strict_slashes=False)
-def python_route(text=None):
-    if text is None:
-        text = 'is cool'
+def python_route(text='is cool'):
     text = text.replace("_", " ")
     return 'Python %s' % text
 
