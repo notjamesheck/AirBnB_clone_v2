9a25da128bdd47349f1dd35558940096f98382e5
notjamesheck <224@holbertonschool.com>
2018-04-19 08:44:36 -0700

    ok

--- models/state.py
+++ models/state.py
@@ -9,6 +9,8 @@ from models import city
 from sqlalchemy import Column, String, Integer, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
 from os import getenv
+import os
+from os import environ
 
 
 class State(BaseModel, Base):
@@ -19,7 +21,7 @@ class State(BaseModel, Base):
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
 
-    if getenv("HBNB_TYPE_STORAGE") == "db":
+    if os.environ.get('HBNB_TYPE_STORAGE') == 'db':
         cities = relationship("City", cascade="all, delete-orphan",
                               backref="state")
 
